2ec6ac1fc418fe59ed00f439fff5b551df1b556e
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-06-25 15:43:24 +0500

    🎨 Style: Add styles for input component

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -1,5 +1,20 @@
+import styles from "./Input.module.scss";
+
 const Input = () => {
-  return <input type="text" />;
+  const currency = "$";
+
+  return (
+    <div className={styles.inputContainer}>
+      <label htmlFor="amount" className={styles.labelText}>
+        Amount:
+      </label>
+
+      <div className={styles.inputField}>
+        {currency}
+        <input type="text" id="amount" defaultValue={15} />
+      </div>
+    </div>
+  );
 };
 
 export default Input;
