a0bbd257bf9489d3e29cb4472227e9d1d86d0af5
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-06-30 12:16:25 +0500

    🚀 Feature: Fix broken error message

--- src/ui/input/instances/ConverterInput.tsx
+++ src/ui/input/instances/ConverterInput.tsx
@@ -32,7 +32,7 @@ const ConverterInput = () => {
   return (
     <div className={styles.converterInput}>
       <Input input={input} setInput={handleInputChange} />
-      {error && <p className={styles.error}>{error}</p>}
+      {error && <p className={styles.errorMessage}>{error}</p>}
     </div>
   );
 };
