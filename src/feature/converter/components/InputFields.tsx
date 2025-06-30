d02378298a637e57288fb00e00f373bf424dd4c1
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-06-30 08:11:55 +0500

    🚀 Feature: Refactor

--- src/feature/converter/components/InputFields.tsx
+++ src/feature/converter/components/InputFields.tsx
@@ -1,17 +1,17 @@
 import styles from "../Converter.module.scss";
 import ConverterInput from "../../../ui/input/instances/ConverterInput.tsx";
-import FromDropdown from "../../../ui/dropdown/instances/FromDropdown.tsx";
 import ConverterSwitchBtn from "../../../ui/switchBtn/instances/ConverterSwitchBtn.tsx";
-import ToDropdown from "../../../ui/dropdown/instances/ToDropdown.tsx";
+import DropdownFrom from "../../../ui/dropdown/instances/DropdownFrom.tsx";
+import DropdownTo from "../../../ui/dropdown/instances/DropdownTo.tsx";
 
 const InputFields = () => {
   return (
     <div className={styles.inputField}>
       <ConverterInput />
       <div className={styles.dropdownsContainer}>
-        <FromDropdown />
+        <DropdownFrom />
         <ConverterSwitchBtn />
-        <ToDropdown />
+        <DropdownTo />
       </div>
     </div>
   );
